f1594809e1bcbbb2061ec0a2746763b73ed9b17a
Arafathislam <islamarafath315@gmail.com>
2022-11-28 20:31:58 +0600

    finish

--- Apiview/Apiview/urls.py
+++ Apiview/Apiview/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from api import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('studentapi/', views.student_api ),
+    path('studentapi/<int:pk>', views.student_api ),
 ]
--- Apiview/api/models.py
+++ Apiview/api/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from django.db.models import CharField,IntegerField
 # Create your models here.
+class Student(models.Model):
+    name=CharField(max_length=100)
+    roll=IntegerField()
+    city=CharField(max_length=100)
